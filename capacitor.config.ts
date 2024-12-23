4cbebfabe87d03689d262c551132c864dab2bcfe
Medhat-Alathmna <medhatalathmna@gmail.com>
2024-12-23 17:17:23 +0200

    0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Tbooking-mobile',
+  appName: 'Tbooking',
   webDir: 'www'
 };
 
